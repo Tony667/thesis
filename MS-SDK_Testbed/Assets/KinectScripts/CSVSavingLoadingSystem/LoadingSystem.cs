e4d5dc733e53c79b6a4e1cce5790a7fb258b89aa
Antonis <antwnis.oikonomou@gmail.com>
2019-06-28 19:19:47 +0300

    loading

--- MS-SDK_Testbed/Assets/KinectScripts/CSVSavingLoadingSystem/LoadingSystem.cs
+++ MS-SDK_Testbed/Assets/KinectScripts/CSVSavingLoadingSystem/LoadingSystem.cs
@@ -95,6 +95,7 @@ public class LoadingSystem : MonoBehaviour
 
     public float[,] animationFloats;
     int nframes = 100000;
+    float timer = 0.1f;
 
 
 
@@ -187,23 +188,46 @@ public class LoadingSystem : MonoBehaviour
     //    }
     //}
 
+
+
     public void Update()
     {
         if (isLoading)
         {
 
-            if (i == 1 && i < numberOfLines)
-            {
-                Debug.Log(i);
-                fields = contents[i].Split(';');
-                i++;
-            }
-
-            //for (int i = 1; i < numberOfLines; i++)
+            //if (i == 1 && i < numberOfLines)
             //{
             //    Debug.Log(i);
             //    fields = contents[i].Split(';');
+            //    i++;
             //}
+
+            for (int i = 1; i < numberOfLines; i++)
+            {
+                Debug.Log(i);
+                fields = contents[i].Split(';');
+                Hip_Center.transform.position = new Vector3(float.Parse(fields[3]), float.Parse(fields[4]), float.Parse(fields[5]));
+                Spine.transform.position = new Vector3(float.Parse(fields[7]), float.Parse(fields[8]), float.Parse(fields[9]));
+                Shoulder_Center.transform.position = new Vector3(float.Parse(fields[11]), float.Parse(fields[12]), float.Parse(fields[13]));
+                Head.transform.position = new Vector3(float.Parse(fields[15]), float.Parse(fields[16]), float.Parse(fields[17]));
+                Shoulder_Left.transform.position = new Vector3(float.Parse(fields[19]), float.Parse(fields[20]), float.Parse(fields[21]));
+                Elbow_Left.transform.position = new Vector3(float.Parse(fields[23]), float.Parse(fields[24]), float.Parse(fields[25]));
+                Wrist_Left.transform.position = new Vector3(float.Parse(fields[27]), float.Parse(fields[28]), float.Parse(fields[29]));
+                Hand_Left.transform.position = new Vector3(float.Parse(fields[31]), float.Parse(fields[32]), float.Parse(fields[33]));
+                Shoulder_Right.transform.position = new Vector3(float.Parse(fields[35]), float.Parse(fields[36]), float.Parse(fields[37]));
+                Elbow_Right.transform.position = new Vector3(float.Parse(fields[39]), float.Parse(fields[40]), float.Parse(fields[41]));
+                Wrist_Right.transform.position = new Vector3(float.Parse(fields[43]), float.Parse(fields[44]), float.Parse(fields[45]));
+                Hand_Right.transform.position = new Vector3(float.Parse(fields[47]), float.Parse(fields[48]), float.Parse(fields[49]));
+                Hip_Left.transform.position = new Vector3(float.Parse(fields[51]), float.Parse(fields[52]), float.Parse(fields[53]));
+                Knee_Left.transform.position = new Vector3(float.Parse(fields[55]), float.Parse(fields[56]), float.Parse(fields[57]));
+                Ankle_Left.transform.position = new Vector3(float.Parse(fields[59]), float.Parse(fields[60]), float.Parse(fields[61]));
+                Foot_Left.transform.position = new Vector3(float.Parse(fields[63]), float.Parse(fields[64]), float.Parse(fields[65]));
+                Hip_Right.transform.position = new Vector3(float.Parse(fields[67]), float.Parse(fields[68]), float.Parse(fields[69]));
+                Knee_Right.transform.position = new Vector3(float.Parse(fields[71]), float.Parse(fields[72]), float.Parse(fields[73]));
+                Ankle_Right.transform.position = new Vector3(float.Parse(fields[75]), float.Parse(fields[76]), float.Parse(fields[77]));
+                Foot_Right.transform.position = new Vector3(float.Parse(fields[79]), float.Parse(fields[80]), float.Parse(fields[81]));
+                isLoading = false;
+            }
 
             //foreach(string line in contents)
             //{
---
+++ MS-SDK_Testbed/Assets/justatest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class justatest : MonoBehaviour
+{
+    public bool isLoading = true;
+
+    private void Update()
+    {
+        if (isLoading)
+        {
+            Test();
+        }
+    }
+    void Test()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+        }
+
+        isLoading = false;
+    }
+}
