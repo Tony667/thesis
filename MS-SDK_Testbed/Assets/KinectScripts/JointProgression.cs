2320857aa604c1006d42f7b3de3b3732cdbf55d2
Antonis <antwnis.oikonomou@gmail.com>
2019-07-04 12:05:40 +0300

    JointProgression Class

---
+++ MS-SDK_Testbed/Assets/KinectScripts/JointProgression.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointProgression
+{
+    private double[] expertMovementHipCenter;
+    private double[] expertMovementSpine;
+    private double[] expertMovementNeck;
+    private double[] expertMovementHead;
+    private double[] expertMovementShoulderCenter;
+    private double[] expertMovementShoulderLeft;
+    private double[] expertMovementElbowLeft;
+    private double[] expertMovementWristLeft;
+    private double[] expertMovementHandLeft;
+    private double[] expertMovementShoulderRight;
+    private double[] expertMovemenElbowRight;
+    private double[] expertMovementWristRight;
+    private double[] expertMovementHandRight;
+    private double[] expertMovementHipLeft;
+    private double[] expertMovementKneeLeft;
+    private double[] expertMovementAnkcleLeft;
+    private double[] expertMovementFootLeft;
+    private double[] expertMovementHipRight;
+    private double[] expertMovementKneeRight;
+    private double[] expertMovementAnckleRight;
+    private double[] expertMovementFootRight;
+
+    private double[] amatureMovementHipCenter;
+    private double[] amatureMovementSpine;
+    private double[] amatureMovementNeck;
+    private double[] amatureMovementShoulderCenter;
+    private double[] amatureMovementShoulderLeft;
+    private double[] amatureMovementElbowLeft;
+    private double[] amatureMovementWristLeft;
+    private double[] amatureMovementHandLeft;
+    private double[] amatureMovementShoulderRight;
+    private double[] amatureMovementElbowRight;
+    private double[] amatureMovementHandRight;
+    private double[] amatureMovementHipLeft;
+    private double[] amatureMovementKneeLeft;
+    private double[] amatureMovementAnkcleLeft;
+    private double[] amatureMovementFootLeft;
+    private double[] amatureMovementHipRight;
+    private double[] amatureMovementKneeRight;
+    private double[] amatureMovementAnkcleRight;
+    private double[] amatureMovementFootRight;
+
+    public List<double> expertJoint0 = new List<double>();
+    public List<double> expertJoint1 = new List<double>();
+    public List<double> expertJoint2 = new List<double>();
+    public List<double> expertJoint3 = new List<double>();
+    public List<double> expertJoint4 = new List<double>();
+    public List<double> expertJoint5 = new List<double>();
+    public List<double> expertJoint6 = new List<double>();
+    public List<double> expertJoint7 = new List<double>();
+    public List<double> expertJoint8 = new List<double>();
+    public List<double> expertJoint9 = new List<double>();
+    public List<double> expertJoint10 = new List<double>();
+    public List<double> expertJoint11 = new List<double>();
+    public List<double> expertJoint12 = new List<double>();
+    public List<double> expertJoint13 = new List<double>();
+    public List<double> expertJoint14 = new List<double>();
+    public List<double> expertJoint15 = new List<double>();
+    public List<double> expertJoint16 = new List<double>();
+    public List<double> expertJoint17 = new List<double>();
+    public List<double> expertJoint18 = new List<double>();
+    public List<double> expertJoint19 = new List<double>();
+
+    public List<double> amatureJoint0 = new List<double>();
+    public List<double> amatureJoint1 = new List<double>();
+    public List<double> amatureJoint2 = new List<double>();
+    public List<double> amatureJoint3 = new List<double>();
+    public List<double> amatureJoint4 = new List<double>();
+    public List<double> amatureJoint5 = new List<double>();
+    public List<double> amatureJoint6 = new List<double>();
+    public List<double> amatureJoint7 = new List<double>();
+    public List<double> amatureJoint8 = new List<double>();
+    public List<double> amatureJoint9 = new List<double>();
+    public List<double> amatureJoint10 = new List<double>();
+    public List<double> amatureJoint11 = new List<double>();
+    public List<double> amatureJoint12 = new List<double>();
+    public List<double> amatureJoint13 = new List<double>();
+    public List<double> amatureJoint14 = new List<double>();
+    public List<double> amatureJoint15 = new List<double>();
+    public List<double> amatureJoint16 = new List<double>();
+    public List<double> amatureJoint17 = new List<double>();
+    public List<double> amatureJoint18 = new List<double>();
+    public List<double> amatureJoint19 = new List<double>();
+
+    public void JointProgressionExpert()
+    {
+        expertMovementHipCenter = expertJoint0.ToArray();
+        //continue
+    }
+
+    public void JointProgressionAmature()
+    {
+        amatureMovementHipCenter = amatureJoint0.ToArray();
+        //continue
+    }
+}
---
+++ MS-SDK_Testbed/Assets/KinectScripts/NewAvatarController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class NewAvatarController : MonoBehaviour
+{
+    // Public variables that will get matched to bones. If empty, the Kinect will simply not track it.
+    public Transform HipCenter;
+    public Transform Spine;
+    public Transform Neck;
+    public Transform Head;
+
+    public Transform LeftClavicle;
+    public Transform LeftUpperArm;
+    public Transform LeftElbow;
+    public Transform LeftHand;
+    private Transform LeftFingers = null;
+
+    public Transform RightClavicle;
+    public Transform RightUpperArm;
+    public Transform RightElbow;
+    public Transform RightHand;
+    private Transform RightFingers = null;
+
+    public Transform LeftThigh;
+    public Transform LeftKnee;
+    public Transform LeftFoot;
+    private Transform LeftToes = null;
+
+    public Transform RightThigh;
+    public Transform RightKnee;
+    public Transform RightFoot;
+    private Transform RightToes = null;
+
+    public Transform BodyRoot;
+    public GameObject OffsetNode;
+
+    public Transform[] bones;
+    public Transform bodyRoot;
+    public GameObject offsetNode;
+
+    public string movementToLoad;
+    public string[] contents;
+    public int numberOfLines;
+    public string[] fields;
+    public int frameNumber = 1;
+
+    // If the bones to be mapped have been declared, map that bone to the model.
+    protected void MapBones()
+    {
+        bones[0] = HipCenter;
+        bones[1] = Spine;
+        bones[2] = Neck;
+        bones[3] = Head;
+
+        bones[4] = LeftClavicle;
+        bones[5] = LeftUpperArm;
+        bones[6] = LeftElbow;
+        bones[7] = LeftHand;
+        bones[8] = LeftFingers;
+
+        bones[9] = RightClavicle;
+        bones[10] = RightUpperArm;
+        bones[11] = RightElbow;
+        bones[12] = RightHand;
+        bones[13] = RightFingers;
+
+        bones[14] = LeftThigh;
+        bones[15] = LeftKnee;
+        bones[16] = LeftFoot;
+        bones[17] = LeftToes;
+
+        bones[18] = RightThigh;
+        bones[19] = RightKnee;
+        bones[20] = RightFoot;
+        bones[21] = RightToes;
+
+        // body root and offset
+        bodyRoot = BodyRoot;
+        //bodyRoot = transform;
+        offsetNode = OffsetNode;
+
+        if (offsetNode == null)
+        {
+            offsetNode = new GameObject(name + "Ctrl") { layer = transform.gameObject.layer, tag = transform.gameObject.tag };
+            offsetNode.transform.position = transform.position;
+            offsetNode.transform.rotation = transform.rotation;
+            offsetNode.transform.parent = transform.parent;
+
+            transform.parent = offsetNode.transform;
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+        }
+
+        //		if(bodyRoot == null)
+        //		{
+        //			bodyRoot = transform;
+        //		}
+    }
+
+    private void Start()
+    {
+        bones = new Transform[22];
+
+        MapBones();
+
+        if (File.Exists(Application.dataPath + "/MovementDataBase/" + movementToLoad + ".csv"))
+        {
+            contents = File.ReadAllLines(Application.dataPath + "/MovementDataBase/" + movementToLoad + ".csv");
+            numberOfLines = contents.Length;
+        }
+    }
+
+    private void Update()
+    {
+        fields = contents[frameNumber].Split(';');
+        HipCenter.transform.position = new Vector3(float.Parse(fields[3]), float.Parse(fields[4]), float.Parse(fields[5]));
+        Spine.transform.position = new Vector3(float.Parse(fields[7]), float.Parse(fields[8]), float.Parse(fields[9]));
+        Neck.transform.position = new Vector3(float.Parse(fields[11]), float.Parse(fields[12]), float.Parse(fields[13]));
+        Head.transform.position = new Vector3(float.Parse(fields[15]), float.Parse(fields[16]), float.Parse(fields[17]));
+        LeftClavicle.transform.position = new Vector3(float.Parse(fields[19]), float.Parse(fields[20]), float.Parse(fields[21]));
+        LeftUpperArm.transform.position = new Vector3(float.Parse(fields[23]), float.Parse(fields[24]), float.Parse(fields[25]));
+        LeftElbow.transform.position = new Vector3(float.Parse(fields[27]), float.Parse(fields[28]), float.Parse(fields[29]));
+        LeftHand.transform.position = new Vector3(float.Parse(fields[31]), float.Parse(fields[32]), float.Parse(fields[33]));
+        RightClavicle.transform.position = new Vector3(float.Parse(fields[35]), float.Parse(fields[36]), float.Parse(fields[37]));
+        RightUpperArm.transform.position = new Vector3(float.Parse(fields[39]), float.Parse(fields[40]), float.Parse(fields[41]));
+        RightElbow.transform.position = new Vector3(float.Parse(fields[43]), float.Parse(fields[44]), float.Parse(fields[45]));
+        RightHand.transform.position = new Vector3(float.Parse(fields[47]), float.Parse(fields[48]), float.Parse(fields[49]));
+        LeftThigh.transform.position = new Vector3(float.Parse(fields[51]), float.Parse(fields[52]), float.Parse(fields[53]));
+        LeftKnee.transform.position = new Vector3(float.Parse(fields[55]), float.Parse(fields[56]), float.Parse(fields[57]));
+        LeftFoot.transform.position = new Vector3(float.Parse(fields[59]), float.Parse(fields[60]), float.Parse(fields[61]));
+        //FootLeft.transform.position = new Vector3(float.Parse(fields[63]), float.Parse(fields[64]), float.Parse(fields[65]));
+        RightThigh.transform.position = new Vector3(float.Parse(fields[67]), float.Parse(fields[68]), float.Parse(fields[69]));
+        RightKnee.transform.position = new Vector3(float.Parse(fields[71]), float.Parse(fields[72]), float.Parse(fields[73]));
+        RightFoot.transform.position = new Vector3(float.Parse(fields[75]), float.Parse(fields[76]), float.Parse(fields[77]));
+        //FootRight.transform.position = new Vector3(float.Parse(fields[79]), float.Parse(fields[80]), float.Parse(fields[81]));
+
+        frameNumber++;
+
+        if (frameNumber >= numberOfLines)
+        {
+            frameNumber = 1;
+        }
+    }
+}
